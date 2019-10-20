2a2682cacfa8c5ca8a91021395cb8ece12579f38
maleem <zizo9585@gmail.com>
2019-10-20 02:13:25 +0100

    bug fix for registration search functionality

--- drugs/views.py
+++ drugs/views.py
@@ -7,8 +7,12 @@ from django.urls import reverse
 
 
 def drug_list(request):
+    #First i call all drugs to be listed, search keyword to blank 
     drugs=Drug.objects.all()
     search_keyword=''
+    #drug_by_no will remain None if search
+    # keyword doesn't match any drug NAFDAC number 
+    drug_by_no=None
 
     if 'title' in request.GET:
         drugs=drugs.order_by('name')
@@ -17,24 +21,38 @@ def drug_list(request):
         drugs=drugs.order_by('-date_approv')
     
     if 'search' in request.GET:
-        search_keyword=request.GET['search']
+        search_keyword=request.GET['search'] #Gets the search keyword
         try:
-            drugs=Drug.objects.filter(reg_no=search_keyword)
+            #checks if keyword matches any drugs NAFDAC number
+            # and drugs_by_no returns a Drug object,  then the
+            # list is then set to None
+            drug_by_no=drugs.get(reg_no=search_keyword)
+            drugs=None
+
+            # if the search keyword doesn't matches any drugs NAFDAC number
+            # then the drugs list stays and is filtered by keyword
         except Drug.DoesNotExist:
             drugs=drugs.filter(name__icontains=search_keyword)
     
     get_dict_copy=request.GET.copy()
     params=get_dict_copy.pop('page',True) and get_dict_copy.urlencode()
-    
-    paginator=Paginator(drugs,50) #paginator object of 50 items per page
-    page=request.GET.get('page') 
-    drugs=paginator.get_page(page)
+
+    # if the drugs list or any of its filter exist
+    # then run pagination 
+    if drugs:
+        paginator=Paginator(drugs,50) #paginator object of 50 items per page
+        page=request.GET.get('page')
+        drugs=paginator.get_page(page)
     
     context={
         'drugs':drugs,
         'params':params,
         'search':search_keyword
     }
+    
+    # if drug_by_no exists, add it to context dict
+    if drug_by_no:
+        context['drug_by_no']=drug_by_no
     return render(request,'drugs/all_drugs.html',context)
 
 # def search(request):
